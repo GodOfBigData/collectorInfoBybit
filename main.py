90ed6e04f3bfa97db7b5c5466fb9f5a29aa37f26
GodOfBigData <stincs369@mail.ru>
2023-02-09 01:38:41 +0300

    change main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from collecting.statistics_collectors import setMainMetric
 from configs.config import api_key, api_secret, proxy, currency, interval
+from time import sleep
 
 
 if __name__ == "__main__":
+    sleep(10)
     setMainMetric(api_key, api_secret, currency, interval, proxy)
